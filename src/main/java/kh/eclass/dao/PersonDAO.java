fecf6a3ba61ea811e9c6498d732539be9f8042a9
tester <test@test.com>
2021-01-07 14:08:43 +0900

    personDAO saved

--- src/main/java/kh/eclass/dao/PersonDAO.java
+++ src/main/java/kh/eclass/dao/PersonDAO.java
@@ -3,11 +3,16 @@ package kh.eclass.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import kh.eclass.dto.PersonDTO;
+
 @Repository
 public class PersonDAO {
 	@Autowired
 	private SqlSession db;
 	
+	public int insert(PersonDTO dto) {
+		return db.insert("Person.insert",dto);
+	}
 	
-	
+	public List
 }
