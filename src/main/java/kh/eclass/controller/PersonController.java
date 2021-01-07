f0e612d90e3efa71f46fcfc2003ddda5eb9977be
Hyokyung <twomoon2411@naver.com>
2021-01-07 14:59:58 +0900

    input 수정 1

--- src/main/java/kh/eclass/controller/PersonController.java
+++ src/main/java/kh/eclass/controller/PersonController.java
@@ -13,11 +13,20 @@ public class PersonController {
 	@Autowired
 	private PersonService service;
 	
+	@RequestMapping("toInput.person")
+	public String toInput() {
+		return "input.jsp";
+	}
 	@RequestMapping("input.person")
 	public String input(String name, String message) {
 		service.input(name, message);
 		System.out.println("이름 : " + name);
 		System.out.println(("메세지 : " + message));
-		return "/";
+		return "home";
+	}
+	
+	@RequestMapping("toOutput.person")
+	public String toOutput() {
+		return "output.jsp";
 	}
 }
