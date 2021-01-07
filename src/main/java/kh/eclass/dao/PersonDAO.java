1d953a896d3d2f28c3babe56df9b6c5c84c77af9
tester <test@test.com>
2021-01-07 14:06:58 +0900

    mybatis part add

---
+++ src/main/java/kh/eclass/dao/PersonDAO.java
@@ -0,0 +1,13 @@
+package kh.eclass.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PersonDAO {
+	@Autowired
+	private SqlSession db;
+	
+	
+	
+}
